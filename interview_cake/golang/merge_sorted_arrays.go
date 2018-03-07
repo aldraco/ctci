8d312828cf643a5866e91ab92021e8b4d58e2799
A.L. Drake <a.l.drake713@gmail.com>
2018-03-06 22:02:10 -0500

    IC merged sorted arrays golang

---
+++ interview_cake/golang/merge_sorted_arrays.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  alice := []int{3, 4, 6, 10, 11, 15}
+  mine := []int{1, 5, 8, 12, 14, 19}
+  fmt.Println(alice)
+  fmt.Println(mine)
+  merged := mergeArrays(alice, mine)
+  fmt.Println(merged)
+}
+
+func mergeArrays(a []int, b []int) []int {
+  merged := make([]int, len(a) + len(b))
+  i := 0;
+  for (len(a) > 0 && len(b) > 0) {
+    if a[0] <= b[0] {
+      merged[i] = a[0]
+      a = a[1:]
+    } else {
+      merged[i] = b[0]
+      b = b[1:]
+    }
+    i++
+  }
+
+  if len(a) > 0 {
+    copy(merged[i:], a)
+  } else {
+    copy(merged[i:], b)
+  }
+  return merged
+}
